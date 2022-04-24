ad6f21441a47171c4e895e4e2c7dafe978cddc75
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-24 12:32:55 -0300

    teste

--- secao4/6_if/script.js
+++ secao4/6_if/script.js
@@ -1,5 +1,14 @@
 let idade = 19;
+nome = 'Luciano';
 
 if(idade > 2){
     console.log('Pode Entrar');
+}
+if(idade < 25){
+    console.log('Idade mair que 25');
+}
+
+let passaporte = true;
+if((nome == 'Luciano' && idade > 30) || passaporte == true){
+    console.log('Liberado!');
 }
