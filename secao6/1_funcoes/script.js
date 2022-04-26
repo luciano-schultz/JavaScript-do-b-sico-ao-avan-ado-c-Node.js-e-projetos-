e6372d16b85126e897b89b0ed2cc5ebe40a2024d
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-26 15:05:29 -0300

    mais sobre função

--- secao6/1_funcoes/script.js
+++ secao6/1_funcoes/script.js
@@ -1,4 +1,4 @@
-function imprimirNoConsole(){
+/*function imprimirNoConsole(){
     console.log("Ola Mundo");
 }
 imprimirNoConsole();
@@ -7,3 +7,20 @@ function imprimirNumero(num){
     console.log("O numero é " + num);
 }
 imprimirNumero(2);
+
+const numeroAleatorio = function(){
+    console.log(Math.random());
+}
+numeroAleatorio();*/
+
+const soma = function(a, b){
+    return a + b;
+}
+console.log(soma(3, 5));
+
+const saudacao = function(nome){
+    if(nome == "Luciano"){
+        return 'Ola Luciano';
+    }
+}
+console.log(saudacao('Luciano'));
