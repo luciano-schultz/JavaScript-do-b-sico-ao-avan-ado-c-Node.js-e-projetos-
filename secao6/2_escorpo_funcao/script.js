aba7f953b60bc14a3b35ca187341fef4940dcc51
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-26 16:13:59 -0300

    mais sobre escopo

---
+++ secao6/2_escorpo_funcao/script.js
@@ -0,0 +1,16 @@
+/*let y = 10;
+
+function imprimir(){
+    let y = 150;
+    console.log(y);
+}
+console.log(y);
+console.log(imprimir());*/
+
+let x = 10;
+if(x > 5){
+    let x = 20
+    x++;
+    console.log(x);
+}
+console.log(x);
