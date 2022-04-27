2bb8b2dcf08a91d281613e438dc5ce82e0f5ed0e
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-27 09:44:07 -0300

    teste

---
+++ exercicios/ex23/script.js
@@ -0,0 +1,8 @@
+function verifica(texto){
+    if(texto.lenght > 10){
+        console.log('texto muito longo');
+    }else{
+        console.log('dentro do limite');
+    }
+}
+verifica('Luciano');
