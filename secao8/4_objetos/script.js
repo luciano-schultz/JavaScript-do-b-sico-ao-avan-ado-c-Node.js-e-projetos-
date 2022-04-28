e6343b3a23c8cd1b9bb453b2c7327c1c821b7f0c
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-28 17:14:27 -0300

    função programar

--- secao8/4_objetos/script.js
+++ secao8/4_objetos/script.js
@@ -2,6 +2,10 @@ let pessoa = {
     nome: 'Luciano',
     profissao: 'Programador',
     idade: 28,
+    programar: function(){
+        console.log('Ola');
+    }
 }
 console.log(pessoa.nome);
 console.log(pessoa.idade);
+pessoa.programar();
