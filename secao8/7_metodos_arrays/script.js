089996eb03c893945e1b4fe7dbb1b59bdd479e25
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-05-04 10:51:10 -0300

    metodos de arrays

---
+++ secao8/7_metodos_arrays/script.js
@@ -0,0 +1,14 @@
+let pessoas = ['Luciano', 'Thalis', 'Leticia'];
+/*
+let pessoaRemovida = pessoas.pop();
+console.log(pessoas);
+console.log(pessoaRemovida);
+pessoas.push('Pedro');
+console.log(pessoas);
+pessoas.shift();
+pessoas.unshift('Schultz');
+console.log(pessoas);
+
+*/
+console.log(pessoas.indexOf('Thalis'));
+console.log(pessoas.lastIndexOf('Leticia'));
