8cf3e3fada28fc0d5c09253e5cbb30e88029f7da
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-05-11 10:39:33 -0300

    get e set

--- secao10/5_construtor_classe/script.js
+++ secao10/5_construtor_classe/script.js
@@ -5,10 +5,21 @@ class Cachorro{
     latir(){
         console.log("Au au");
     }
+    get verRaca(){
+        return 'A raça é ' + this.raca;
+    }
+    set novaRaca(value){
+        this.raca = value;
+    }
 }
+
 Cachorro.prototype.patas = 4;
 let cor = Symbol();
 Cachorro.prototype[cor] = 'preto';
 let labrador = new Cachorro('Labrador');
+let golden = new Cachorro('pastor Alemão');
+golden.novaRaca = 'Golden';
+
+console.log(golden.verRaca);
 console.log(labrador.patas);
 console.log(Cachorro.prototype[cor]);
